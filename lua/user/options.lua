822d3d54ce7163a7fb21ff909e3e19aab3ce330e
novel2430 <catmanfong2430@qq.com>
2023-03-02 00:01:04 +0800

    syn on 2023-03-02 00:01:01 CST

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -10,13 +10,13 @@ vim.opt.ignorecase = true                       -- ignore case in search pattern
 vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
 --vim.opt.pumheight = 10                          -- pop up menu height
 --vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
-vim.opt.showtabline = 2                         -- always show tabs
+-- vim.opt.showtabline = 2                         -- always show tabs
 --vim.opt.smartcase = true                        -- smart case
 --vim.opt.smartindent = true                      -- make indenting smarter again
---vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
---vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
+vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
+vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = false                        -- creates a swapfile
--- vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
+vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true                         -- enable persistent undo
 vim.opt.updatetime = 300                        -- faster completion (4000ms default)
@@ -27,16 +27,20 @@ vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
 vim.opt.cursorline = true                       -- highlight the current line
 vim.opt.number = true                           -- set numbered lines
 --vim.opt.relativenumber = false                  -- set relative numbered lines
---vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
+vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
 --vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
 --vim.opt.wrap = false                            -- display lines as one long line
 --vim.opt.scrolloff = 8                           -- is one of my fav
 --vim.opt.sidescrolloff = 8
 --vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
-vim.opt.termguicolors = true
+vim.opt.winbar = '%f'
+
+
 
 --vim.opt.shortmess:append "c"
 
 --vim.cmd "set whichwrap+=<,>,[,],h,l"
 --vim.cmd [[set iskeyword+=-]]
 --vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+
